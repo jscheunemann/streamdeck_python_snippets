55d3643fd6f085f83f779db3fa568b53e8b78053
Jason Scheunemann <jason.scheunemann@gmail.com>
2024-06-09 12:33:52 -0400

    Cleared clipboard prior to req.text

--- pbcopy_from_website.py
+++ pbcopy_from_website.py
@@ -7,6 +7,7 @@ import sys
 
 req = requests.get(sys.argv[1])
 
+subprocess.run("pbcopy", text=True, input='')
 subprocess.run("pbcopy", text=True, input=req.text)
 
 pyautogui.hotkey('command', 'v', interval=0.25)
